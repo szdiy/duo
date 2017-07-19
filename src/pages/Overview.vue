66ceb8ce4186b4ee1286c403eb2370b84ce3c63f
liangzenghai <liangzenghai@jd.com>
2017-07-19 11:29:10 +0800

    add charts

--- src/pages/Overview.vue
+++ src/pages/Overview.vue
@@ -5,27 +5,32 @@
   	<div class="row placeholders">
   	  <div class="col-xs-6 col-sm-3 placeholder">
   	    <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-  	    <h4>Label</h4>
+  	    <h4>今日用电</h4>
+  	    <span class="text-muted">Something else</span>
+  	  </div>
+  	  <div class="col-xs-6 col-sm-3 placeholder">
+        <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+        <h4>昨日用电</h4>
+        <span class="text-muted">Something else</span>
+      </div>
+  	  <div class="col-xs-6 col-sm-3 placeholder">
+  	    <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+  	    <h4>本周用电</h4>
   	    <span class="text-muted">Something else</span>
   	  </div>
   	  <div class="col-xs-6 col-sm-3 placeholder">
   	    <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-  	    <h4>Label</h4>
-  	    <span class="text-muted">Something else</span>
-  	  </div>
-  	  <div class="col-xs-6 col-sm-3 placeholder">
-  	    <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-  	    <h4>Label</h4>
-  	    <span class="text-muted">Something else</span>
-  	  </div>
-  	  <div class="col-xs-6 col-sm-3 placeholder">
-  	    <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-  	    <h4>Label</h4>
+  	    <h4>本月用电</h4>
   	    <span class="text-muted">Something else</span>
   	  </div>
   	</div>
 
-  	<h2 class="sub-header">一些中文</h2>
+    <h2 class="sub-header">Recent Data</h2>
+    <div class="table-responsive">
+      <highcharts :options='{"chart":{"type":"column"},"title":{"text":"Daily Power Usage"},"subtitle":{"text":"Source: Project Duo report"},"xAxis":{"categories":["7/8","7/9","7/10","7/11","7/12","7/13","7/14","7/15","7/16","7/17","7/18","7/19"],"crosshair":true},"yAxis":{"min":0,"title":{"text":"Usage (kW/h)"}},"plotOptions":{"column":{"pointPadding":0.2,"borderWidth":0}},"series":[{"name":"Shenzhen Office","data":[49.9,71.5,106.4,129.2,144,176,135.6,148.5,216.4,194.1,95.6,54.4]}]}'></highcharts>
+    </div>
+
+  	<h2 class="sub-header">Recent Data</h2>
   	<div class="table-responsive">
   	  <table class="table table-striped">
   	    <thead>
@@ -157,6 +162,9 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import VueHighcharts from 'vue-highcharts'
+Vue.use(VueHighcharts)
 export default {
   name: 'index'
 }
