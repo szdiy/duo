ce69e4e01e167c32246ef21920cef6cd1bd79d2c
Terry Ouyang <terry.ouyang@gmail.com>
2017-09-29 10:39:19 +0800

    fix last report timing bug

--- src/api/LastReports.js
+++ src/api/LastReports.js
@@ -1,64 +1,66 @@
-export default [{
-  id: 5146,
-  time: 1500650440,
-  arriveAt: 1500650445,
-  data: 3156.00
-}, {
-  id: 5146,
-  time: 1500650440,
-  arriveAt: 1500650445,
-  data: 3156.00
-}, {
-  id: 5146,
-  time: 1500650440,
-  arriveAt: 1500650445,
-  data: 3156.00
-}, {
-  id: 5146,
-  time: 1500650440,
-  arriveAt: 1500650445,
-  data: 3156.00
-}, {
-  id: 5146,
-  time: 1500650440,
-  arriveAt: 1500650445,
-  data: 3156.00
-}, {
-  id: 5146,
-  time: 1500650440,
-  arriveAt: 1500650445,
-  data: 3156.00
-}, {
-  id: 5146,
-  time: 1500650440,
-  arriveAt: 1500650445,
-  data: 3156.00
-}, {
-  id: 5146,
-  time: 1500650440,
-  arriveAt: 1500650445,
-  data: 3156.00
-}, {
-  id: 5146,
-  time: 1500650440,
-  arriveAt: 1500650445,
-  data: 3156.00
-}, {
-  id: 5146,
-  time: 1500650440,
-  arriveAt: 1500650445,
-  data: 3156.00
-}, {
-  id: 5146,
-  time: 1500650440,
-  arriveAt: 1500650445,
-  data: 3156.00
-}, {
-  id: 5146,
-  time: 1500650440,
-  arriveAt: 1500650445,
-  data: 3156.00
-}]
+// export default [{
+//   id: 5146,
+//   time: 1500650440,
+//   arriveAt: 1500650445,
+//   data: 3156.00
+// }, {
+//   id: 5146,
+//   time: 1500650440,
+//   arriveAt: 1500650445,
+//   data: 3156.00
+// }, {
+//   id: 5146,
+//   time: 1500650440,
+//   arriveAt: 1500650445,
+//   data: 3156.00
+// }, {
+//   id: 5146,
+//   time: 1500650440,
+//   arriveAt: 1500650445,
+//   data: 3156.00
+// }, {
+//   id: 5146,
+//   time: 1500650440,
+//   arriveAt: 1500650445,
+//   data: 3156.00
+// }, {
+//   id: 5146,
+//   time: 1500650440,
+//   arriveAt: 1500650445,
+//   data: 3156.00
+// }, {
+//   id: 5146,
+//   time: 1500650440,
+//   arriveAt: 1500650445,
+//   data: 3156.00
+// }, {
+//   id: 5146,
+//   time: 1500650440,
+//   arriveAt: 1500650445,
+//   data: 3156.00
+// }, {
+//   id: 5146,
+//   time: 1500650440,
+//   arriveAt: 1500650445,
+//   data: 3156.00
+// }, {
+//   id: 5146,
+//   time: 1500650440,
+//   arriveAt: 1500650445,
+//   data: 3156.00
+// }, {
+//   id: 5146,
+//   time: 1500650440,
+//   arriveAt: 1500650445,
+//   data: 3156.00
+// }, {
+//   id: 5146,
+//   time: 1500650440,
+//   arriveAt: 1500650445,
+//   data: 3156.00
+// }]
+
+export default [] // # default empty data
 
 import moment from 'moment'
 import { secondsToMillis } from '../utils/datetime'
@@ -88,11 +90,11 @@ export function parseLastReports (powerDetailList) {
   }
 
   const records = powerDetailList.reduce((daySum, dayRecord) => {
-    return daySum.concat(dayRecord ? dayRecord.detail.map(entryRecordToOption) : [])
+    return daySum.concat(dayRecord ? dayRecord.detail : [])
   }, [])
 
   // force sort once, by time descending
   records.sort((x1, x2) => (x2.time - x1.time))
 
-  return records.slice(0, 50)
+  return records.slice(0, 50).map(entryRecordToOption)
 }
